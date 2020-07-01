f0408109f368ae4f6b37962a96020237c500778c
mckay <kmotseki@gmail.com>
2020-07-01 22:28:48 +0200

    With errors from lecture47

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
-# from django.utils.translation import gettext as _
+from django.utils.translation import gettext as _
 
 from core import models
 
@@ -10,18 +10,17 @@ class UserAdmin(BaseUserAdmin):
     list_display = ['email', 'name']
     fieldsets = (
         (None, {'fields': ('email', 'password')}),
-        (('Personal Info'), {'fields': ('name',)}),
+        (_('Personal Info'), {'fields': ('name',)}),
         (
-            ('Permissions'),
-            {'fields': ('is_active', 'is_staff', 'is_superuser')}
+            _('Permissions'),
+            {'fields': ('is_active', 'is_staff', 'is_superuser',)}
         ),
-        (('Important Dates'), {'fields': ('last_login',)})
+        (_('Important dates'), {'fields': ('last_login',)}),
     )
-
     add_fieldsets = (
         (None, {
-            'classes': ('wide'),
-            'fields': ('email', 'password', 'password2')
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2')
         }),
     )
 
